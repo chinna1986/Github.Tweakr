d5b244257325eacc36dfc64a4b1ed902a26dd345
Chandu <chandra.vedantham@gmail.com>
2014-03-20 22:29:58 -0400

    Added code to change the  link href

---
+++ ext/src/inject/inject.js
@@ -0,0 +1,11 @@
+var cloneInDesktopButton = $("a[href^='github-windows']");
+if(cloneInDesktopButton.length > 0){
+	var cloneUrl = $("div.clone-url-box input.clone.js-url-field:visible").val();
+	var cloneInDesktopUrlParts = cloneInDesktopButton.attr("href").split("github-windows://openRepo/");
+	if(cloneUrl.length > 0 && cloneInDesktopUrlParts.length == 2&& (cloneInDesktopUrlParts[1] !== cloneUrl)) {
+		cloneInDesktopUrlParts[0] =  "github-windows://openRepo/";
+		cloneInDesktopUrlParts[1] = cloneUrl;
+		console.log(cloneInDesktopUrlParts);
+		cloneInDesktopButton.attr("href", cloneInDesktopUrlParts.join(""))
+	}
+}
